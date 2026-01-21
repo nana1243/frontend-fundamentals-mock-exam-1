d35c5c0d64f2730743b2e7a2c04a38d0fa6d791a
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-01-21 14:38:38 +0900

    fea) savingResults 관련 UI 컴포넌트화

--- src/components/SavingResults/index.tsx
+++ src/components/SavingResults/index.tsx
@@ -1,48 +1,40 @@
 import { Border, colors, ListHeader, ListRow, Spacing } from 'tosslib';
 import useSavingsStore from '../../store/savings/useSavingsStore';
+import useSavingsProduct from '../../api/savings/savings.query';
+import { useMemo } from 'react';
+import SavingExpectedResults from '../SavingExpectedResults';
+import SavingRecommendResults from '../SavingRecommendResults';
 
-const SavingResults = () => {
+interface SavingResultsProps {
+  selectedProductId?: string;
+}
 
+const SavingResults = (props: SavingResultsProps) => {
+  const { selectedProductId } = props;
+  const { data } = useSavingsProduct(selectedProductId);
+  const { getRecommendProduct } = useSavingsProduct();
+  const { getExpectedSavingResults } = useSavingsStore();
 
+  const expectedResults = useMemo(() => {
+    return getExpectedSavingResults(data?.annualRate);
+  }, [data?.annualRate, getExpectedSavingResults]);
+
+  const listRowConfigData = [
+    { top: '예상 수익 금액', bottom: `${expectedResults.expectedTotalAmount.toLocaleString()}원` },
+    { top: '목표 금액과의 차이', bottom: `${expectedResults.difference.toLocaleString()}원` },
+    { top: '추천 월 납입 금액', bottom: `${expectedResults.recommendedMonthlyAmount.toLocaleString()}원` },
+  ];
+  const recommendProducts = useMemo(() => {
+    return getRecommendProduct(expectedResults.monthlyAmount, expectedResults.period);
+  }, [expectedResults.monthlyAmount, expectedResults.period, getRecommendProduct]);
+
+  console.log('recommendProducts', recommendProducts);
 
   return (
     <>
       <Spacing size={8} />
 
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="2RowTypeA"
-            top="예상 수익 금액"
-            topProps={{ color: colors.grey600 }}
-            bottom={`1,000,000원`}
-            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
-          />
-        }
-      />
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="2RowTypeA"
-            top="목표 금액과의 차이"
-            topProps={{ color: colors.grey600 }}
-            bottom={`-500,000원`}
-            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
-          />
-        }
-      />
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="2RowTypeA"
-            top="추천 월 납입 금액"
-            topProps={{ color: colors.grey600 }}
-            bottom={`100,000원`}
-            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
-          />
-        }
-      />
-
+      <SavingExpectedResults selectedProductId={selectedProductId} listRowConfigData={listRowConfigData} />
       <Spacing size={8} />
       <Border height={16} />
       <Spacing size={8} />
@@ -50,36 +42,7 @@ const SavingResults = () => {
       <ListHeader title={<ListHeader.TitleParagraph fontWeight="bold">추천 상품 목록</ListHeader.TitleParagraph>} />
       <Spacing size={12} />
 
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="3RowTypeA"
-            top={'기본 정기적금'}
-            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-            middle={`연 이자율: 3.2%`}
-            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-            bottom={`100,000원 ~ 500,000원 | 12개월`}
-            bottomProps={{ fontSize: 13, color: colors.grey600 }}
-          />
-        }
-        onClick={() => {}}
-      />
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="3RowTypeA"
-            top={'고급 정기적금'}
-            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-            middle={`연 이자율: 2.8%`}
-            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-            bottom={`50,000원 ~ 1,000,000원 | 24개월`}
-            bottomProps={{ fontSize: 13, color: colors.grey600 }}
-          />
-        }
-        onClick={() => {}}
-      />
-
-      <Spacing size={40} />
+      <SavingRecommendResults recommendProducts={recommendProducts} />
     </>
   );
 };
