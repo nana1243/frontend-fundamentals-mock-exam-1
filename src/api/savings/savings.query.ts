945c801647f30799a3b15cc1a1812f8ac171a992
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-01-21 14:36:38 +0900

    fea) query를 통해 서버 로직 응집화

--- src/api/savings/savings.query.ts
+++ src/api/savings/savings.query.ts
@@ -1,9 +1,31 @@
 import { useQuery } from '@tanstack/react-query';
 import { getProducts } from './savings.api';
 
-export default function useSavingsProduct() {
-  return useQuery({
+export default function useSavingsProduct(productId?: string) {
+  const { data, isLoading, isFetching, error } = useQuery({
     queryKey: ['savings-products'],
     queryFn: getProducts,
+    staleTime: 1000 * 60 * 5,
+    select: data => {
+      if (productId) {
+        return data.find(product => product.id === productId);
+      }
+      return data;
+    },
   });
+  const getRecommendProduct = (monthlyAmount?: number, period?: number) => {
+    if (!data || !monthlyAmount || !period) {
+      return null;
+    }
+
+    return data.find(product => product.minMonthlyAmount <= monthlyAmount && product.availableTerms >= period) || null;
+  };
+
+  return {
+    data,
+    isLoading,
+    isFetching,
+    error,
+    getRecommendProduct,
+  };
 }
